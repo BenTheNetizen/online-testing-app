72f8c0c8118a413d42818deab56c8f41ff831432
Carlos <admin@celestialallure.com>
2021-08-12 20:48:25 -0400

    v1

--- register/forms.py
+++ register/forms.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 
 #to add more fields to the registration form, add more attributes and update the 'fields' property in class Meta
 class RegisterForm(UserCreationForm):
-    
+    email = forms.EmailField()
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
