5d93827c937064a6e05f46cd654f4d73927a4d1a
Benjamin <benjamin.chen@yale.edu>
2022-10-09 01:02:23 -0400

    Completed early math versino of problem database

--- exams/static/problem_database.js
+++ exams/static/problem_database.js
@@ -1,6 +1,6 @@
 // constants from html document
-const sectionBox = document.getElementById('section-box');
-const sectionForm = document.getElementById('section-form');
+const questionBox = document.getElementById('section-box');
+const sectionForm = document.getElementById('problem-database-form');
 const csrf = document.getElementsByName('csrfmiddlewaretoken')[0].value;
 const url = window.location.href 
 
@@ -29,12 +29,29 @@ function getProblemData(category) {
         }
       */
       console.log(data);
+
+      // reset the section box and answer box
+      questionBox.innerHTML = '';
+      if (!data.length) {
+        console.log('no data');
+        questionBox.innerHTML = 'No questions found for this category';
+      }
       data.forEach((el, index) => {
         // let imgHTML = '<img class="math-material" src="${questionData[3]}"/>'
-        sectionBox.innerHTML += `
-        <div class='question-container question-container-math' id="${index}-text">
-        <div class="mb-2 testing">
-          <b class="ca-question-data">${el.text}</b>
+        // questionBox.innerHTML += `
+        // <div class='question-container question-container-math' id="${index}-text">
+        // `;
+        if (el.materialUrl) {
+          questionBox.innerHTML += `
+          <img class="math-material" src="${el.materialUrl}">
+          `;
+        }
+        questionBox.innerHTML += `
+        <div class="mb-2 testing">  
+          <b class="ca-question-data">
+            ${el.text}
+            <img class='question-hide-show-img' src=${hideAnswerUrl} data-index=${index} data-correct-answer=${el.correctAnswer} alt="eye-icon" onclick="showCorrectAnswer(this)" />
+          </b>
         </div>
         <div class="answers-container" id="${index}-answers"></div>
         `;
@@ -45,9 +62,10 @@ function getProblemData(category) {
         if (el.choices) {
           el.choices.forEach(choice => {
             let answer = choice.text
+            let letter = choice.letter
             answerBox.innerHTML += `
             <label for="${el.text}" class="answer-container" onclick="radioChecked(this, ${index})">
-              <input type="radio" class="ans" id="${el.text}-${answer}" name="${el.text}" value="${answer}">
+              <input type="radio" class="ans" id="${index}-${letter}" name="${el.text}" value="${answer}">
               <span class="checkmark-con"><span class="material-icons checkmark">done</span></span>
             ${answer}
             </label>
@@ -57,9 +75,42 @@ function getProblemData(category) {
           console.log('WE HAVE A FREE RESPONSE QUESTION');
         }
       })
+
+      questionBox.innerHTML += `
+      </div>
+      </div>
+      `
+      // $('.answers-container').css('width', '50%')
+      // needed statement for mathjax to render
+      MathJax.typesetPromise();
+
+      // set styling for the button
+      // first set all buttons to grey
+      let buttons = document.getElementsByName('problem-categories-button');
+      buttons.forEach(button => {
+        button.className = 'problem-categories-button';
+      })
+      // now set styling on selected button
+      let selectedButton = document.querySelector(`[data-category=${CSS.escape(category)}]`);
+      selectedButton.className = 'problem-categories-button-selected';
     },
     error: function(error) {
       console.log(error);
     }
   })
+}
+
+function showCorrectAnswer(el) {
+  let index = el.dataset.index;
+  let correctAnswer = el.dataset.correctAnswer;
+  if (el.src.includes(hideAnswerUrl)) {
+    el.src = showAnswerUrl;
+    // get's the input element of the correct answer and checks it
+    document.getElementById(`${index}-${correctAnswer}`).checked = true;
+  } else {
+    el.src = hideAnswerUrl;
+    document.getElementById(`${index}-${correctAnswer}`).checked = false;
+  }
+  
+  // write functionality for showing the correct answer
 }
